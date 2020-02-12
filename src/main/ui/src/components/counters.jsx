b0724ca808eda443ec8a1fec27e41ede6d40c9f6
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-02-13 00:41:56 +0530

    Add ui

---
+++ src/main/ui/src/components/counters.jsx
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import Counter from "./counter";
+
+class Counters extends Component {
+    state = {
+        counters: [
+            { id: 1, value: 4},
+            { id: 2, value: 0},
+            { id: 3, value: 0},
+            { id: 4, value: 0},
+        ]
+    };
+    handleDelete = (counterId) => {
+       const counters = this.state.counters.filter(c => c.id !== counterId);
+       this.setState({ counters });
+    };
+
+    handleIncrement = (counter) => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({counters});
+    };
+
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({counters});
+    };
+    render() {
+        return (
+            <div>
+                <button
+                    onClick={this.handleReset}
+                    className="btn btn-primary btn-sm m-2">Reset</button>
+                { this.state.counters.map(counter => (
+                    <Counter
+                        key={counter.id}
+                        onIncrement={this.handleIncrement}
+                        onDelete={this.handleDelete}
+                        counter = {counter}
+                    >
+                    </Counter>
+                        ))}
+            </div>
+        );
+    }
+}
+
+export default Counters;
---
+++ src/main/ui/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+import Counters from "./components/counters";
+
+ReactDOM.render(<Counters />, document.getElementById('root'));
