407ee57c0e45d58654766fc441925ed13d9b4f62
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-02-13 22:04:02 +0530

    Update ui, port from jsx to bare tsx react app

--- src/main/ui/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.min.css"
-import Counters from "./components/counters";
-
-ReactDOM.render(<Counters />, document.getElementById('root'));
