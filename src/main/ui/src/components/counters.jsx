089586d36fe481c98bc241ead903ca27896d6df7
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-02-13 15:18:52 +0530

    Remove one counter for demo

--- src/main/ui/src/components/counters.jsx
+++ src/main/ui/src/components/counters.jsx
@@ -4,7 +4,6 @@ import Counter from "./counter";
 class Counters extends Component {
     state = {
         counters: [
-            { id: 1, value: 4},
             { id: 2, value: 0},
             { id: 3, value: 0},
             { id: 4, value: 0},
