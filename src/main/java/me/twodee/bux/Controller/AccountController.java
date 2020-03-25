4f9a20982dc4f31545479acd157abf50f9ec995b
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-03-25 13:56:45 +0530

    Return HTTP CREATED on successful registration

--- src/main/java/me/twodee/bux/Controller/AccountController.java
+++ src/main/java/me/twodee/bux/Controller/AccountController.java
@@ -3,36 +3,34 @@ package me.twodee.bux.Controller;
 import me.twodee.bux.DTO.HelperValueObject.Notification;
 import me.twodee.bux.DTO.UserDTO;
 import me.twodee.bux.Model.Entity.User;
-import me.twodee.bux.Model.Service.Account;
+import me.twodee.bux.Model.Service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 @CrossOrigin(maxAge = 3600)
 @RestController
 public class AccountController
 {
-    private final Account account;
+    private final AccountService accountService;
 
     @Autowired
-    public AccountController(Account account)
+    public AccountController(AccountService accountService)
     {
-        this.account = account;
+        this.accountService = accountService;
     }
 
     @PostMapping("/accounts/create")
     public ResponseEntity<Notification> createNewAccount(@RequestBody UserDTO dto)
     {
-        if (!account.usersExist())
-            account.register(dto, User.Role.ADMIN);
+        if (!accountService.usersExist())
+            accountService.register(dto, User.Role.ADMIN);
         else
-            account.register(dto, User.Role.STANDARD);
+            accountService.register(dto, User.Role.STANDARD);
 
         if (dto.getNotification().hasErrors())
             return new ResponseEntity<>(dto.getNotification(), HttpStatus.BAD_REQUEST);
-
-        return ResponseEntity.ok(dto.getNotification());
+        return new ResponseEntity<>(dto.getNotification(), HttpStatus.CREATED);
     }
 }
