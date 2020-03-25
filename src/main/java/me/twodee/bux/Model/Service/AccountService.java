188600a885b695a3811660d8915875cb42db23eb
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-03-25 23:44:49 +0530

    Add login support with jwt token generation

--- src/main/java/me/twodee/bux/Model/Service/AccountService.java
+++ src/main/java/me/twodee/bux/Model/Service/AccountService.java
@@ -1,33 +1,42 @@
 package me.twodee.bux.Model.Service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import me.twodee.bux.DTO.HelperValueObject.Error;
 import me.twodee.bux.DTO.HelperValueObject.Notification;
-import me.twodee.bux.Util.MessageByLocaleService;
-import me.twodee.bux.Util.DomainToDTOConverter;
-import me.twodee.bux.DTO.UserDTO;
+import me.twodee.bux.DTO.User.UserDTO;
+import me.twodee.bux.DTO.User.UserLoginDTO;
 import me.twodee.bux.Model.Entity.User;
 import me.twodee.bux.Model.Repository.UserRepository;
+import me.twodee.bux.Provider.LocaleMessagingValidationProvider;
+import me.twodee.bux.Util.CryptoUtil;
+import me.twodee.bux.Util.DomainToDTOConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import javax.validation.ConstraintViolation;
-import javax.validation.Validator;
+import java.security.Key;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+
 
 @Service
 public class AccountService
 {
     private final UserRepository repository;
-    private final MessageByLocaleService messageByLocaleService;
-    private final Validator validator;
+    private final LocaleMessagingValidationProvider provider;
 
     @Autowired
-    public AccountService(UserRepository repository, MessageByLocaleService messageByLocaleService, Validator validator)
+    public AccountService(UserRepository repository,
+                          LocaleMessagingValidationProvider provider)
     {
         this.repository = repository;
-        this.messageByLocaleService = messageByLocaleService;
-        this.validator = validator;
+        this.provider = provider;
     }
 
     public void register(UserDTO dto, User.Role role)
@@ -36,13 +45,15 @@ public class AccountService
             User user = new User(dto.getName(), dto.getUsername(), dto.getEmail(), dto.getPassword());
             user.setRole(role);
 
-            Set<ConstraintViolation<User>> violations = validator.validate(user);
+            Set<ConstraintViolation<User>> violations = provider.getValidator().validate(user);
             dto.setNotification(DomainToDTOConverter.convert(violations));
-            if (!dto.getNotification().hasErrors())
+            if (!dto.getNotification().hasErrors()) {
                 dto.appendNotification(checkForDuplicates(dto.getUsername(), dto.getEmail()));
+            }
 
-            if (!dto.getNotification().hasErrors())
+            if (!dto.getNotification().hasErrors()) {
                 repository.save(user);
+            }
 
         } catch (DataIntegrityViolationException e) {
             // This is mainly for race conditions, uniqueness should be checked above beforehand.
@@ -50,23 +61,68 @@ public class AccountService
             // Can't do it while being DB-agnostic, let it fail with a generic message, let the user try again
             // Can show the proper error then
             Notification notification = new Notification();
-            notification.addError(new Error("global", messageByLocaleService.getMessage("validation.exception")));
+            notification.addError(
+                    new Error("global", provider.getMessageByLocaleService().getMessage("validation.exception")));
             dto.setNotification(notification);
         }
+    }
+
+    public String login(UserLoginDTO dto)
+    {
+        Set<ConstraintViolation<UserLoginDTO>> violations = provider.getValidator().validate(dto);
+        if (violations.isEmpty()) {
+            Optional<User> user = repository.findUserByEmailOrUsername(dto.getIdentifier(), dto.getIdentifier());
+            if (user.isPresent()) {
+                return getJWTStringIfValidPassword(user.get(), dto);
+            }
+            dto.setNotification(createNotificationForInvalidLogin("identifier"));
+        }
+        else {
+            dto.setNotification(DomainToDTOConverter.convert(violations));
+        }
+
+        return null;
+    }
+
+    private String getJWTStringIfValidPassword(User user, UserLoginDTO dto)
+    {
+        if (CryptoUtil.verifyPassword(dto.getPassword(), user.getHashedPassword())) {
+            Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+            return Jwts.builder().setSubject("Joe").signWith(key).compact();
+        }
+        dto.setNotification(createNotificationForInvalidLogin("password"));
+        return null;
+    }
+
+    private Notification createNotificationForInvalidLogin(String cause)
+    {
+        Notification notification = new Notification();
+
+        Map<String, String> causeMap = new HashMap<>();
+        causeMap.put("identifier",
+                     provider.getMessageByLocaleService().getMessage("validation.login.identifier.invalid"));
+        causeMap.put("password",
+                     provider.getMessageByLocaleService().getMessage("validation.login.password.invalid"));
+
+        notification.addError(new Error(cause, causeMap.get(cause)));
+        return notification;
     }
 
     private Notification checkForDuplicates(String username, String email)
     {
         Notification notification = new Notification();
         if (repository.findUserByUsername(username).isPresent()) {
-            notification.addError(new Error("username", messageByLocaleService.getMessage("validation.username.taken")));
+            notification.addError(new Error("username", provider.getMessageByLocaleService().getMessage(
+                    "validation.username.taken")));
         }
         if (repository.findUserByEmail(email).isPresent()) {
-            notification.addError(new Error("email", messageByLocaleService.getMessage("validation.email.taken")));
+            notification.addError(
+                    new Error("email", provider.getMessageByLocaleService().getMessage("validation.email.taken")));
         }
 
         return notification;
     }
+
     public boolean usersExist()
     {
         return repository.count() > 0;
--- src/test/java/me/twodee/bux/Model/Service/AccountServiceTest.java
+++ src/test/java/me/twodee/bux/Model/Service/AccountServiceTest.java
@@ -1,6 +1,8 @@
 package me.twodee.bux.Model.Service;
 
-import me.twodee.bux.DTO.UserDTO;
+import me.twodee.bux.DTO.User.UserDTO;
+import me.twodee.bux.DTO.User.UserLoginDTO;
+import me.twodee.bux.Provider.LocaleMessagingValidationProvider;
 import me.twodee.bux.Util.MessageByLocaleService;
 import me.twodee.bux.Model.Entity.User;
 import me.twodee.bux.Model.Repository.UserRepository;
@@ -17,7 +19,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -28,6 +30,9 @@ class AccountServiceTest
     UserRepository repository;
 
     @Mock
+    LocaleMessagingValidationProvider provider;
+
+    @Mock
     MessageByLocaleService messageByLocaleService;
 
     Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
@@ -36,12 +41,14 @@ class AccountServiceTest
     void setUp()
     {
         MockitoAnnotations.initMocks(this);
-
+        when(provider.getValidator()).thenReturn(validator);
+        when(provider.getMessageByLocaleService()).thenReturn(messageByLocaleService);
     }
+
     @Test
     void registerFailure()
     {
-        AccountService accountService = new AccountService(repository, messageByLocaleService, validator);
+        AccountService accountService = new AccountService(repository, provider);
         UserDTO dto = new UserDTO("", "", "", "");
 
         accountService.register(dto, User.Role.ADMIN);
@@ -52,7 +59,7 @@ class AccountServiceTest
     @Test
     void testReturnedErrors()
     {
-        AccountService accountService = new AccountService(repository, messageByLocaleService, validator);
+        AccountService accountService = new AccountService(repository, provider);
         UserDTO dto = new UserDTO("d", "jdoe123", "2d@", "helloworld1234");
         accountService.register(dto, User.Role.ADMIN);
 
@@ -68,7 +75,7 @@ class AccountServiceTest
     @Test
     void registerSuccess()
     {
-        AccountService accountService = new AccountService(repository, messageByLocaleService, validator);
+        AccountService accountService = new AccountService(repository, provider);
         UserDTO dto = new UserDTO("John Doe", "jdoe", "jd@jd.co", "helloworld1234");
 
         when(repository.save(any(User.class))).thenReturn(new User());
@@ -81,7 +88,7 @@ class AccountServiceTest
     @Test
     void testDuplicateRegistration()
     {
-        AccountService accountService = new AccountService(repository, messageByLocaleService, validator);
+        AccountService accountService = new AccountService(repository, provider);
         UserDTO dto = new UserDTO("John Doe", "jdoe", "jd@jd.co", "helloworld1234");
 
         when(repository.findUserByUsername(anyString())).thenReturn(Optional.of(new User()));
@@ -96,18 +103,76 @@ class AccountServiceTest
         assertThat(errors.get("username"), equalTo("validation.username.taken"));
         assertThat(errors.get("email"), equalTo("validation.email.taken"));
     }
+
     @Test
     void testDuplicateRaceCondition()
     {
-        AccountService accountService = new AccountService(repository, messageByLocaleService, validator);
+        AccountService accountService = new AccountService(repository, provider);
         UserDTO dto = new UserDTO("John Doe", "jdoe", "jd@jd.co", "helloworld1234");
 
         when(repository.save(any(User.class))).thenThrow(new DataIntegrityViolationException("error"));
-        when(messageByLocaleService.getMessage("validation.exception")).thenReturn("Something went wrong, please try again");
+        when(messageByLocaleService.getMessage("validation.exception")).thenReturn(
+                "Something went wrong, please try again");
         accountService.register(dto, User.Role.ADMIN);
 
         assertTrue(dto.getNotification().hasErrors());
         assertThat(dto.getNotification().getErrors().get("global"),
                    equalTo("Something went wrong, please try again"));
+    }
+
+    @Test
+    void loginSuccess()
+    {
+        AccountService accountService = new AccountService(repository, provider);
+        UserLoginDTO dto = new UserLoginDTO("dedipyaman", "qwerty1234");
+        Optional<User> user = Optional.of(new User("John", "dedipyaman", "2d@twodee.me", "qwerty1234"));
+
+        when(repository.findUserByEmailOrUsername(anyString(), anyString())).thenReturn(user);
+
+        System.out.println(accountService.login(dto));
+        assertThat(accountService.login(dto), is(not(emptyString())));
+    }
+
+    @Test
+    void loginWithEmptyPassword()
+    {
+        AccountService accountService = new AccountService(repository, provider);
+        UserLoginDTO dto = new UserLoginDTO("dedipyaman", "");
+
+        assertNull(accountService.login(dto));
+        assertTrue(dto.getNotification().hasErrors());
+        assertThat(dto.getNotification().getErrors().size(), equalTo(1));
+        assertThat(dto.getNotification().getErrors().get("password"), equalTo("{validation.login.password.invalid}"));
+    }
+
+    @Test
+    void loginWithInvalidPassword()
+    {
+        AccountService accountService = new AccountService(repository, provider);
+        UserLoginDTO dto = new UserLoginDTO("dedipyaman", "helloworld");
+
+        Optional<User> user = Optional.of(new User("John", "dedipyaman", "2d@twodee.me", "qwerty1234"));
+        when(repository.findUserByEmailOrUsername(anyString(), anyString())).thenReturn(user);
+        when(messageByLocaleService.getMessage("validation.login.password.invalid")).thenReturn("{validation.login.password.invalid}");
+
+        assertNull(accountService.login(dto));
+        assertTrue(dto.getNotification().hasErrors());
+        assertThat(dto.getNotification().getErrors().size(), equalTo(1));
+        assertThat(dto.getNotification().getErrors().get("password"), equalTo("{validation.login.password.invalid}"));
+    }
+
+    @Test
+    void loginWithNonexistent()
+    {
+        AccountService accountService = new AccountService(repository, provider);
+        UserLoginDTO dto = new UserLoginDTO("dedipyaman", "helloworld");
+
+        when(repository.findUserByEmailOrUsername(anyString(), anyString())).thenReturn(Optional.empty());
+        when(messageByLocaleService.getMessage("validation.login.identifier.invalid")).thenReturn("{validation.login.identifier.invalid}");
+
+        assertNull(accountService.login(dto));
+        assertTrue(dto.getNotification().hasErrors());
+        assertThat(dto.getNotification().getErrors().size(), equalTo(1));
+        assertThat(dto.getNotification().getErrors().get("identifier"), equalTo("{validation.login.identifier.invalid}"));
     }
 }
