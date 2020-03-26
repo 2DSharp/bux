0f71e6c33f446e3bd33fe005e15005cd402f11b2
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-03-27 02:27:05 +0530

    Redirect all requests other than api/resources to react's index.html

--- src/main/java/me/twodee/bux/BuxApplication.java
+++ src/main/java/me/twodee/bux/BuxApplication.java
@@ -2,12 +2,27 @@ package me.twodee.bux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
-public class BuxApplication
+public class BuxApplication implements ErrorController
 {
     public static void main(String[] args)
     {
         SpringApplication.run(BuxApplication.class, args);
+    }
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "forward:/index.html";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
     }
 }
