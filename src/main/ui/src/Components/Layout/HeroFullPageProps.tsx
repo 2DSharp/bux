d17721320d9afa783cb2ef011f643374a41fb901
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-03-28 15:22:27 +0530

    Add logo on top of card

--- src/main/ui/src/Components/Layout/HeroFullPageProps.tsx
+++ src/main/ui/src/Components/Layout/HeroFullPageProps.tsx
@@ -12,7 +12,9 @@ const HeroFullPage = (props: HeroFullPageProps) => {
             <section className="hero is-light is-fullheight">
                 <div className="hero-body">
                     <div className="container">
-                        <p className="title has-text-centered">{props.title}</p>
+                        <p className="title has-text-centered">
+                            <img style={{ height: 40 }} src={process.env.PUBLIC_URL + '/bux_big.png'} alt="Bux" />
+                        </p>
                         <div className="columns is-centered">
                             <div className={`column is-${props.width}`}>
                                 {props.children}
