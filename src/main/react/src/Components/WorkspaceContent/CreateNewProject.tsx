0d941eed8c9c2dc04f1e54b051aa42c546dcea69
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-03-29 17:39:03 +0530

    Update new project creation wizard to be on a blank hero styled page

--- src/main/react/src/Components/WorkspaceContent/CreateNewProject.tsx
+++ src/main/react/src/Components/WorkspaceContent/CreateNewProject.tsx
@@ -1,74 +1,44 @@
 import React from 'react';
-import ReactDOM from "react-dom";
-
-const portalRoot = document.getElementById("modal-root") as Element;
+import HeroFullPage from "../Layout/HeroFullPageProps";
+import "../../sass/card-form.scss"
 
 const CreateNewProject = () => {
-    return ReactDOM.createPortal(
-        <div className="modal is-active">
-            <div className="modal-background"/>
-            <div className="modal-card">
-                <header className="modal-card-head">
-                    <strong className="modal-card-title">Create a new project</strong>
-                    <button className="delete" aria-label="close"/>
-                </header>
-                <section className="modal-card-body">
-
-                    <div className="field">
-                        <label className="label">Project Name</label>
-                        <div className="control has-icons-left has-icons-right">
-                            <input className="input is-success" type="text"
-                                   placeholder="Choose a unique name, for example: Bux"/>
-                            <span className="icon is-small is-left">
+    return (
+        <HeroFullPage width={4} title={"Create a new project"} alignLeft={true}>
+            <>
+                <div className="field">
+                    <label className="label">Choose a name for your project</label>
+                    <div className="control has-icons-left has-icons-right">
+                        <input className="input is-success" type="text"
+                               placeholder="Choose a unique name, for example: Bux"/>
+                        <span className="icon is-small is-left">
                                 <i className="mdi mdi-developer-board"/>
                             </span>
-                            <span className="icon is-small is-right">
+                        <span className="icon is-small is-right">
                                 <i className="fas fa-check"/>
                             </span>
-                        </div>
-                        <p className="help is-success">This project name is available</p>
                     </div>
-                    <div className="field is-grouped">
-                        <div className="control">
-                            <label className="label">Project Key</label>
-                            <div className="control has-icons-left has-icons-right">
-                                <input className="input is-danger" type="text"
-                                       placeholder="Select a short unique project identifier"/>
-                                <span className="icon is-small is-left">
+                </div>
+                <div className="field is-grouped">
+                    <div className="control">
+                        <label className="label">Project Key</label>
+                        <div className="control has-icons-left has-icons-right">
+                            <input className="input is-danger" type="text"
+                                   placeholder="Select a short unique project identifier"/>
+                            <span className="icon is-small is-left">
                                 <i className="mdi mdi-key"/>
                             </span>
-                                <span className="icon is-small is-right">
+                            <span className="icon is-small is-right">
                                 <i className="mdi mdi-exclamation-thick"/>
                             </span>
-                                <p className="help is-danger">The key has already been taken</p>
-                            </div>
-                        </div>
-                        <div className="control">
-                            <label className="label">Team</label>
-                            <div className="control">
-                                <div className="select is-rounded">
-                                    <select>
-                                        <option>Select a team</option>
-                                    </select>
-                                </div>
-                            </div>
+                            <p className="help is-danger">The key has already been taken</p>
                         </div>
                     </div>
-                    <div className="field">
-                        <label className="label">Description</label>
-                        <div className="control">
-                            <textarea className="textarea has-fixed-size"
-                                      placeholder="Add a short description for your new project"/>
-                        </div>
-                    </div>
-                </section>
-                <footer className="modal-card-foot">
-                    <button className="button is-primary">Save changes</button>
-                    <button className="button">Cancel</button>
-                </footer>
-            </div>
-        </div>,
-        portalRoot
+
+                </div>
+            </>
+        </HeroFullPage>
+
     );
 };
 
