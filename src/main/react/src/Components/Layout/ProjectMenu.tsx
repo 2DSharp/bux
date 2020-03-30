096ea4d96ce43ed3e1f1a4b1533bcc339a500df4
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-03-31 03:35:26 +0530

    De-clutter project menu by dynamically generating the menu

--- src/main/react/src/Components/Layout/ProjectMenu.tsx
+++ src/main/react/src/Components/Layout/ProjectMenu.tsx
@@ -2,20 +2,67 @@ import * as React from 'react';
 import 'bulma/css/bulma.css'
 import '../../sass/base.scss'
 import styles from '../../sass/menu.module.scss';
+import classNames from "classnames";
+
+interface MenuItem {
+    active?: boolean,
+    text: string,
+    icon: string
+}
 
 const ProjectMenu = () => {
+    const menu = [
+        {
+            active: true,
+            text: "Dashboard",
+            icon: "mdi-math-compass"
+        },
+        {
+            text: "Issues",
+            icon: "mdi-bug"
+        },
+        {
+            text: "Tasks",
+            icon: "mdi-view-grid"
+        },
+        {
+            text: "Teams",
+            icon: "mdi-account-group"
+        },
+        {
+            text: "Docs",
+            icon: "mdi-text"
+        },
+        {
+            text: "Progress",
+            icon: "mdi-chart-bell-curve-cumulative"
+        },
+        {
+            text: "Settings",
+            icon: "mdi-cog"
+        }
+    ];
+    const itemClass = (active: boolean | undefined) => classNames({
+        "is-active": active,
+        [styles.isActive]: active
+    });
+    const generateMenu = (item: MenuItem) => {
+        return (
+            <li className={styles.item}>
+                <a className={itemClass(item.active)}>
+                    <span className={`icon ${styles.iconContainer}`}>
+                        <i className={`mdi ${styles.icon} ${item.icon}`}/>
+                    </span>
+                    {item.text}
+                </a>
+            </li>
+        );
+    };
     return (
         <div className={`hero column sticky is-2 is-fullheight ${styles.fixedColumn}`}>
             <aside className={`menu  ${styles.menu}`}>
-
                 <ul className="menu-list">
-                    <li className={styles.item}><a className={`is-active ${styles.isActive}`}><span className={`icon ${styles.icon}`}><i className="mdi mdi-math-compass"/> </span>Dashboard</a></li>
-                    <li className={styles.item}><a><span className={`icon ${styles.icon}`}><i className="mdi mdi-bug"/> </span>Issues</a></li>
-                    <li className={styles.item}><a><span className={`icon ${styles.icon}`}><i className="mdi mdi-view-grid"/> </span>Tasks</a></li>
-                    <li className={styles.item}><a><span className={`icon ${styles.icon}`}><i className="mdi mdi-account-group"/> </span>Teams</a></li>
-                    <li className={styles.item}><a><span className={`icon ${styles.icon}`}><i className="mdi mdi-text"/> </span>Docs</a></li>
-                    <li className={styles.item}><a><span className={`icon ${styles.icon}`}><i className="mdi mdi-chart-bell-curve-cumulative"/> </span>Progress</a></li>
-                    <li className={styles.item}><a><span className={`icon ${styles.icon}`}><i className="mdi mdi-cog"/> </span>Settings</a></li>
+                    { menu.map(generateMenu) }
                 </ul>
             </aside>
         </div>
