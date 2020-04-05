8536ccfc5d9adf06e14938adf14b214975083341
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-04-05 23:57:13 +0530

    Add global configuration for CORS
    TODO: Fix origins in production

---
+++ src/main/java/me/twodee/bux/Configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package me.twodee.bux.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer
+{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedOrigins("http://localhost:3000");
+    }
+}
+
