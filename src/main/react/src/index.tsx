07e750e17148a049b298ff0570d8a4c6c64172fd
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-04-06 00:01:19 +0530

    Add credentials support globally for accepting cookies

--- src/main/react/src/index.tsx
+++ src/main/react/src/index.tsx
@@ -8,7 +8,7 @@ import App from "./Components/App";
 axios.defaults.baseURL = 'http://localhost:2310/api/';
 //axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
 axios.defaults.headers.post['Content-Type'] = 'application/text';
-
+axios.defaults.withCredentials = true;
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
