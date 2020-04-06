dcf3abafa2e90990271f281451ec467b3c932ee5
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-04-06 23:43:16 +0530

    Restructure into HeroPages

--- src/main/react/src/Components/WorkspaceContent/CreateNewProject.tsx
+++ src/main/react/src/Components/WorkspaceContent/CreateNewProject.tsx
@@ -1,35 +1,36 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import HeroFullPage from "../Layout/HeroFullPageProps";
 import "../../sass/card-form.scss"
 import TextField from "../Element/TextField";
 import InputContainer from "../Element/InputContainer";
 
+const modalRoot = document.getElementById("modal-root") as Element;
+
 const CreateNewProject = () => {
     return (
-        <HeroFullPage width={4} title={"Create a new project"} alignLeft={true}>
-            <>
-                <InputContainer>
-                    <TextField error={false} placeholder={"Example: Bux"}
-                               label={"Project name"} required={true}
-                    />
-                </InputContainer>
+            <HeroFullPage width={4} title={"Create a new project"} alignLeft={true}>
+                <>
+                    <InputContainer>
+                        <TextField error={false} placeholder={"Example: Bux"}
+                                   label={"Project name"} required={true}
+                        />
+                    </InputContainer>
 
-                <InputContainer>
-                    <>
-                        <TextField error={false} placeholder={"A short 2-8 character identifier"}
-                                   label={"Project key"} required={true}/>
+                    <InputContainer>
+                        <>
+                            <TextField error={false} placeholder={"A short 2-8 character identifier"}
+                                       label={"Project key"} required={true}/>
 
-                        <span className="help"><i className="mdi mdi-information-outline"/> The key is a unique identifier for a project. This will be used to track your issues, logs etc.
+                            <span className="help"><i className="mdi mdi-information-outline"/> The key is a unique identifier for a project. This will be used to track your issues, logs etc.
                     Eg.: Cloud Server can be keyed as CS. </span>
-                    </>
-                </InputContainer>
-                <button className="button is-primary">
-                    Create
-                </button>
-            </>
-        </HeroFullPage>
-
-    );
-};
+                        </>
+                    </InputContainer>
+                    <button className="button is-primary">
+                        Create
+                    </button>
+                </>
+            </HeroFullPage>
+            )};
 
 export default CreateNewProject;
--- src/main/react/src/Components/WorkspaceContent/EmptyProjectsPrompt.tsx
+++ src/main/react/src/Components/WorkspaceContent/EmptyProjectsPrompt.tsx
@@ -1,27 +1,29 @@
 import React from 'react';
+import HeroFullPage from "../Layout/HeroFullPageProps";
+import {Link} from "react-router-dom";
+
 interface EmptyProjectsPrompt {
     onClick: () => void
 }
-const EmptyProjectsPrompt = (props : EmptyProjectsPrompt) => {
+
+const EmptyProjectsPrompt = (props: EmptyProjectsPrompt) => {
     return (
-        <section className="hero is-light is-fullheight-with-navbar">
-            <div className="hero-body">
-                <div className="container has-text-centered">
-                    <h1 className="title">
-                       You don't have any projects yet
-                    </h1>
-                    <h2 className="subtitle">
-                        Get started by creating a new project.
-                    </h2>
-                    <a onClick={props.onClick} className="button is-primary">
+        <HeroFullPage width={5} title={"You don't have any projects yet"}>
+            <div className="container has-text-centered">
+                <h2 className="subtitle">
+                    Get started by creating a new project.
+                </h2>
+                <Link to="/new/project">
+                    <button className="button is-primary">
                         <span className="icon">
                             <i className="mdi mdi-18px mdi-plus" aria-hidden="true"/>
                         </span>
                         <strong>Create</strong>
-                    </a>
-                </div>
+                    </button>
+                </Link>
             </div>
-        </section>
+        </HeroFullPage>
+
     );
 };
 
