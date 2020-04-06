4aaee7bf44758666dc112081e0ac109b06c0792f
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-04-06 18:17:02 +0530

    Remove filtering from resources, exclude Webconfig from production build

--- src/main/java/me/twodee/bux/Configuration/WebConfig.java
+++ src/main/java/me/twodee/bux/Configuration/WebConfig.java
@@ -4,9 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * This will be excluded from the production build
+ * This is only for enabling CORS to work with react for development
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer
 {
+    /**
+     * Add cors for development with react server on 3000
+     * @param registry Cors registry mappings
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
