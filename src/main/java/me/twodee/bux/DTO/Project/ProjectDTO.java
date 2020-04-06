75089361c1e473e5d0d750c171abb8542e662117
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-04-06 23:39:49 +0530

    Keep json field naming strategy to default for consistency

--- src/main/java/me/twodee/bux/DTO/Project/ProjectDTO.java
+++ src/main/java/me/twodee/bux/DTO/Project/ProjectDTO.java
@@ -1,20 +1,21 @@
 package me.twodee.bux.DTO.Project;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import me.twodee.bux.DTO.DataTransferObject;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
 public class ProjectDTO extends DataTransferObject
 {
     private String name;
-    private String key;
+    private String projectKey;
 
-    public ProjectDTO(String name, String key)
+    public ProjectDTO(String name, String projectKey)
     {
         this.name = name;
-        this.key = key;
+        this.projectKey = projectKey;
     }
 
     public ProjectDTO() {}
-
 }
