0e78e7c4271a8fcf5c3d7ad95fa58aded0d01278
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-04-06 23:42:30 +0530

    Put in useEffect for one time REST call, remove new project creation logic

--- src/main/react/src/Components/Page/Projects.tsx
+++ src/main/react/src/Components/Page/Projects.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 
 import EmptyProjectsPrompt from "../WorkspaceContent/EmptyProjectsPrompt";
 import Axios from "axios";
@@ -7,21 +7,20 @@ import CreateNewProject from "../WorkspaceContent/CreateNewProject";
 const Projects = () => {
     const [projects, setProjects] = useState('');
     const [showNewProjectWizard, setShowNewProjectWizard] = useState(false);
-    Axios.get('/projects')
-        .then(response => {
-            setProjects(response.data);
-        });
+    useEffect(() => {
+        Axios.get('/projects')
+            .then(response => {
+                setProjects(response.data);
+            });
+    }, []);
 
     const getProjectsContent = () => {
-        if (showNewProjectWizard)
-            return <CreateNewProject/>;
-
-        else if (projects.length == 0)
+        if (projects.length == 0)
             return <EmptyProjectsPrompt onClick={() => setShowNewProjectWizard(true)}/>
     };
     return (
         <>
-            {getProjectsContent()}
+            { getProjectsContent() }
         </>
     );
 };
