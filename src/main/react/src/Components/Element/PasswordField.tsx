d8a7bf140c06fe06c93966fd630288aa0050ed07
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-04-11 20:36:54 +0530

    Fix eyeStatus in PasswordField.tsx

--- src/main/react/src/Components/Element/PasswordField.tsx
+++ src/main/react/src/Components/Element/PasswordField.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import TextField, {TextFieldProps} from "./TextField";
+import cx from "classnames";
 
 const PasswordField = (props: TextFieldProps) => {
     const [passwordType, setPasswordType] = useState("password");
@@ -11,16 +12,22 @@ const PasswordField = (props: TextFieldProps) => {
             setPasswordType("password");
         }
     };
+    const eyeStatus = cx({
+        mdi: true,
+        'active-gray': true,
+        'mdi-eye': passwordType == "password",
+        'mdi-eye-off': passwordType == "text"
+    });
     return (
         <TextField
-                   errorMsg={props.errorMsg}
-                   forwardRef={props.forwardRef}
-                   placeholder={props.placeholder} name={props.name}
-                   leftIcon={props.leftIcon}
-                   type={passwordType}
-                   rightIcon="mdi-eye"
-                   onChange={props.onChange}
-                   rightIconClickable={true}
+            errorMsg={props.errorMsg}
+            forwardRef={props.forwardRef}
+            placeholder={props.placeholder} name={props.name}
+            leftIcon={props.leftIcon}
+            type={passwordType}
+            rightIcon={eyeStatus}
+            onChange={props.onChange}
+            rightIconClickable={true}
                    onRightIconClick={togglePassword}
                    label={props.label}
         />
