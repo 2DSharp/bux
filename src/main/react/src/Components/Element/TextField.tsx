89ea382f52e2448c95158482fcdea52ed0c11141
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-04-12 01:01:55 +0530

    Put control inside a field div

--- src/main/react/src/Components/Element/TextField.tsx
+++ src/main/react/src/Components/Element/TextField.tsx
@@ -4,7 +4,7 @@ import cx from "classnames";
 export interface TextFieldProps extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
     type?: string;
     success?: boolean;
-    errorMsg?: string;
+    errorMsg?: string | undefined;
     leftIcon?: string
     hasRightErrorIcon?: boolean
     rightIcon?: string
@@ -42,31 +42,33 @@ const TextField = (props: TextFieldProps) => {
         "clickable": props.rightIconClickable
     });
     return (
-        <div className={controlClass}>
+        <div className="field">
             {props.label &&
             <label className="label">{props.label} {props.required && <span style={{color: "red"}}>*</span>}</label>
             }
-            <input type={(props.type) ? props.type : "text"} placeholder={props.placeholder} name={props.name}
-                   className={inputClass}
-                   onChange={props.onChange}
-                   autoComplete={props.autoComplete}
-                   ref={props.forwardRef}
-            />
-            {props.leftIcon &&
-            <span className="icon is-small is-left">
+            <div className={controlClass}>
+
+                <input type={(props.type) ? props.type : "text"} placeholder={props.placeholder} name={props.name}
+                       className={inputClass}
+                       onChange={props.onChange}
+                       autoComplete={props.autoComplete}
+                       ref={props.forwardRef}
+                />
+                {props.leftIcon &&
+                <span className="icon is-small is-left">
                 <i className={`mdi ${props.leftIcon}`}/>
             </span>
-            }
-            {((hasError && props.hasRightErrorIcon) || props.rightIcon) &&
+                }
+                {((hasError && props.hasRightErrorIcon) || props.rightIcon) &&
 
-            <span onClick={props.onRightIconClick} className={rightIconContainerClass}>
+                <span onClick={props.onRightIconClick} className={rightIconContainerClass}>
                 <i className={`${rightIconClass} ${(props.rightIconClickable) ? props.rightIcon : "mdi-exclamation-thick"}`}/>
             </span>
-            }
-            {hasError &&
-            <p className="help is-danger">{props.errorMsg}</p>
-            }
-
+                }
+                {hasError &&
+                <p className="help is-danger">{props.errorMsg}</p>
+                }
+            </div>
         </div>
     );
 };
