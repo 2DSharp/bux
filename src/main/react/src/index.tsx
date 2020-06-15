129e6a7566b9f431fcb94cf7e34c60ffa41f890d
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-06-15 16:02:57 +0530

    Clean up boilerplate

--- src/main/react/src/index.tsx
+++ src/main/react/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
 import App from "./Components/App";
