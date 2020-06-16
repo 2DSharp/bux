0bb30de4190bb6fc3a65be94b334d3151646803f
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-06-16 22:41:06 +0530

    Add @RestController to GoalController

--- src/main/java/me/twodee/bux/Controller/GoalController.java
+++ src/main/java/me/twodee/bux/Controller/GoalController.java
@@ -11,10 +11,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
 
+@RestController
 public class GoalController extends RestAPI {
     private AccountService accountService;
     private GoalService goalService;
