779813011e200cc93de4cee42e32fdb8b59c39eb
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-06-17 00:37:39 +0530

    At DateTime formatting utils

--- src/main/java/me/twodee/bux/Util/BaseUtil.java
+++ src/main/java/me/twodee/bux/Util/BaseUtil.java
@@ -2,13 +2,14 @@ package me.twodee.bux.Util;
 
 import me.twodee.bux.Interface.ThrowingFunction;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.function.Function;
 
 public class BaseUtil
 {
     public static <T, R> Function<T, R> throwingFunctionWrapper(
-            ThrowingFunction<T, R, Throwable> throwingFunction)
-    {
+            ThrowingFunction<T, R, Throwable> throwingFunction) {
         return i -> {
             try {
                 return throwingFunction.apply(i);
@@ -16,5 +17,15 @@ public class BaseUtil
                 throw new RuntimeException(ex);
             }
         };
+    }
+
+    public static LocalDateTime strToLocalDateTime(String dateString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return LocalDateTime.parse(dateString, formatter);
+    }
+
+    public static String localDateTimeToStr(LocalDateTime dateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return dateTime.format(formatter);
     }
 }
