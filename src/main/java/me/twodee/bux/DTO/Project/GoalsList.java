d10bbeb658348685085b7693fad990705687c0c2
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-06-18 00:26:14 +0530

    Include empty array

--- src/main/java/me/twodee/bux/DTO/Project/GoalsList.java
+++ src/main/java/me/twodee/bux/DTO/Project/GoalsList.java
@@ -1,12 +1,10 @@
 package me.twodee.bux.DTO.Project;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
 @Getter
 @AllArgsConstructor
 public class GoalsList {
