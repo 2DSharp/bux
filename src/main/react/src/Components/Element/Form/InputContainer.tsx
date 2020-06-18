4bf5e340f227750ef12aba3b306947fdfbe0a0ea
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-06-18 23:56:19 +0530

    Allow inlining input containers

--- src/main/react/src/Components/Element/Form/InputContainer.tsx
+++ src/main/react/src/Components/Element/Form/InputContainer.tsx
@@ -3,10 +3,11 @@ import React, {ReactNode} from 'react';
 interface InputContainerProps {
     children: ReactNode,
     small?: boolean
+    inline?: boolean
 }
 const InputContainer = (props: InputContainerProps) => {
     return (
-        <div className={`field vertically-spaced ${props.small && 'small'}`}>
+        <div className={`field vertically-spaced ${props.small ? 'small' : ''} ${props.inline ? 'inline' : ''}`}>
             {props.children}
         </div>
     );
