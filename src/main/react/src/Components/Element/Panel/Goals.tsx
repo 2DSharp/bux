ff64ab3bbf87a6ffef0ffef8b2aec526046d7ab7
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-06-19 18:27:46 +0530

    Reformat

--- src/main/react/src/Components/Element/Panel/Goals.tsx
+++ src/main/react/src/Components/Element/Panel/Goals.tsx
@@ -99,46 +99,46 @@ const Goals = (props: { projectKey: string }) => {
                     <div className={classes.panelContainer}>
                         {
                             goals[activeTab] ? (
-                                goals[activeTab].length == 0
-                                    ? <div style={{margin: 10, fontSize: 14, color: "gray", textAlign: "center"}}>
-                                        <div style={{width: 250, margin: "0 auto"}}>
-                                            <SharedGoalsPlaceholder style={{width: 250, height: 200}}/>
+                                    goals[activeTab].length == 0
+                                        ? <div style={{margin: 10, fontSize: 14, color: "gray", textAlign: "center"}}>
+                                            <div style={{width: 250, margin: "0 auto"}}>
+                                                <SharedGoalsPlaceholder style={{width: 250, height: 200}}/>
+                                            </div>
+                                            This area seems empty. Create a new goal to get started!
                                         </div>
-                                        This area seems empty. Create a new goal to get started!
-                                    </div>
-                                    : goals[activeTab].map(goal => (
-                                        <a className="panel-block">
+                                        : goals[activeTab].map(goal => (
+                                            <a className="panel-block">
                                 <span className="panel-icon">
                                     <Priority type={goal.priority}/>
                                 </span>
-                                            <div className={classes.panelItem}>
-                                                <div>
-                                                    {goal.title}
-                                                    {goal.milestone &&
-                                                    <span style={{
-                                                        fontSize: 13,
-                                                        color: "darkgray",
-                                                        maxWidth: 100,
-                                                        display: "inline-block",
-                                                        float: "right",
-                                                        marginRight: 10
-                                                    }}>
+                                                <div className={classes.panelItem}>
+                                                    <div>
+                                                        {goal.title}
+                                                        {goal.milestone &&
+                                                        <span style={{
+                                                            fontSize: 13,
+                                                            color: "darkgray",
+                                                            maxWidth: 100,
+                                                            display: "inline-block",
+                                                            float: "right",
+                                                            marginRight: 10
+                                                        }}>
                                                 <MdIcon value="mdi-flag-checkered"/>{goal.milestone}</span>
-                                                    }
-                                                </div>
-                                                <div className={classes.statsContainer}>
-                                                    <span className={classes.stat}><b>Deadline: </b>{goal.deadline}</span>
-                                                    <span className={classes.stat} style={{float: "right"}}>
+                                                        }
+                                                    </div>
+                                                    <div className={classes.statsContainer}>
+                                                        <span className={classes.stat}><b>Deadline: </b>{goal.deadline}</span>
+                                                        <span className={classes.stat} style={{float: "right"}}>
                                             <progress
                                                 className={progressPressure(goal.pressure)}
                                                 value={goal.progress}
                                                 max="100">{goal.progress}%</progress>
                                              <> {goal.progress}%</>
                                         </span>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </a>
-                                    ))
+                                            </a>
+                                        ))
                                 ) :
                                 <div style={{height: 150}} className="centered-absolutely">
                                     <Spin/>
@@ -154,7 +154,7 @@ const Goals = (props: { projectKey: string }) => {
                     </div>
                 </nav>
             </div>
-            <NewGoal visible={modalVisible} setModalVisible={setModalVisible}/>
+            <NewGoal project={props.projectKey} visible={modalVisible} setModalVisible={setModalVisible}/>
         </>
     );
 };
