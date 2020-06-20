7fd952d33d5b0a85a1b65b1112c78698d30f4b0a
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-06-21 00:02:40 +0530

    Fix links

--- src/main/react/src/Components/WorkspaceContent/Project.tsx
+++ src/main/react/src/Components/WorkspaceContent/Project.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ContentWithMenu from "../Layout/ContentWithMenu";
 import ProjectMenu from "../Layout/ProjectMenu";
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import Dashboard from "./Dashboard";
 
 const Project = () => {
@@ -12,8 +12,8 @@ const Project = () => {
             <div className="project-container">
                 <nav className="breadcrumb" aria-label="breadcrumbs">
                     <ul>
-                        <li><a href="#">Projects</a></li>
-                        <li><a href="#">{id}</a></li>
+                        <li><Link to="/projects">Projects</Link></li>
+                        <li><Link to={`/projects/${id}`}>{id}</Link></li>
                         <li className="is-active"><a href="#" aria-current="page">Dashboard</a></li>
                     </ul>
                 </nav>
