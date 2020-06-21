a102de7e01482dd6b104e073209fdee5c0e2927a
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-06-21 14:18:26 +0530

    Convert to jsx for quick-fixing props conflicts

--- src/main/react/src/Components/Element/Form/ExpandingTextArea.jsx
+++ src/main/react/src/Components/Element/Form/ExpandingTextArea.jsx
@@ -1,24 +1,19 @@
-import React, {ChangeEvent} from 'react';
+import React from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 import {makeStyles} from "@material-ui/styles";
 import {withFormData} from "./FormData";
-
-interface ExpandingTextAreaInterface extends React.DetailedHTMLProps<React.TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement> {
-    onChange: any
-    maxRows: number
-}
 
 const useStyles = makeStyles({
     root: {
         minHeight: "3em !important"
     }
 });
-const ExpandingTextArea = (props: ExpandingTextAreaInterface) => {
+const ExpandingTextArea = (props) => {
     const classes = useStyles();
     return (
         <TextareaAutosize {...props} maxRows={props.maxRows} placeholder={props.placeholder}
                           className={`textarea has-fixed-size ${classes.root}`}
-                          onChange={(event: ChangeEvent<HTMLTextAreaElement>) => props.onChange(event.target.value)}
+                          onChange={(event) => props.onChange(event.target.value)}
         >
             {props.children}
         </TextareaAutosize>
