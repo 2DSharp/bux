41c406e0046db86b661aca2d82ec1aa35b7f1be1
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-06-23 01:52:49 +0530

    Add dynamic form for quick-adding a task

--- src/main/react/src/Components/Element/Table/DnDTable.tsx
+++ src/main/react/src/Components/Element/Table/DnDTable.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Draggable, Droppable} from "react-beautiful-dnd";
 import {makeStyles} from "@material-ui/styles";
 import {TaskData} from "../../../types";
 import Priority from "../Icon/Priority";
 import variables from "../../../sass/colors.module.scss";
+import TextField from "../Form/TextField";
+import PrioritySelector from "../Form/PrioritySelector";
+import DatePickerField from "../Form/DatePickerField";
+import moment from 'moment';
+import MdIcon from "../Icon/MDIcon";
 
 interface DnDTableData {
-    id: string
+    id: string,
 }
 
 interface DnDTableProps {
     data: DnDTableData
-    tasks: TaskData[]
+    tasks: TaskData[],
+    displayAdded?: boolean
+
 }
 
 const useStyles = makeStyles({
     id: {
-        width: 20
+        width: 20,
+        textAlign: "center"
     },
     priority: {
-        width: 20
+        width: 80
     },
     deadline: {
         width: 60
@@ -29,12 +37,16 @@ const useStyles = makeStyles({
     },
     row: {
         backgroundColor: "#fff",
-        "&:hover": {
-            backgroundColor: "rgb(0, 0, 255, 0.1)"
-        }
     },
     table: {
         border: `1px solid ${variables.borderColor}`,
+    },
+    editable: {
+        height: 30,
+    },
+    check: {
+        color: variables.primary,
+        cursor: "pointer"
     }
 });
 const TaskRow = (props: { data: TaskData, index: number }) => {
@@ -45,8 +57,9 @@ const TaskRow = (props: { data: TaskData, index: number }) => {
                 className={classes.row}>
                 <td className={classes.id}>{props.data.id}</td>
                 <td className={classes.title}>{props.data.title}</td>
-                <td className={classes.priority}><Priority type={props.data.priority}/></td>
-                <td className={classes.deadline}>25-07-2020</td>
+                <td className={classes.priority} style={{textAlign: "center"}}><Priority type={props.data.priority}/>
+                </td>
+                <td className={classes.deadline} style={{textAlign: "center"}}>25-07-2020</td>
             </tr>
         }
     </Draggable>
@@ -54,17 +67,40 @@ const TaskRow = (props: { data: TaskData, index: number }) => {
 
 const DnDTable = (props: DnDTableProps) => {
     const classes = useStyles();
+    const [saveIconHover, setSaveIconHover] = useState(false);
     return (
-        <table className={`table container is-fluid is-narrow is-hoverable ${classes.table}`}>
+        <table className={`table container is-fluid is-hoverable ${classes.table}`}>
 
             <Droppable droppableId={props.data.id}>
                 {provided =>
                     <tbody {...provided.droppableProps} ref={provided.innerRef}>
+                    {props.displayAdded &&
+                    <tr className={classes.row} style={{backgroundColor: "rgba(135, 206, 235, 0.2)"}}>
+                        <td style={{textAlign: "center"}} className={classes.id}>
+                            <MdIcon onMouseOver={() => setSaveIconHover(true)}
+                                    onMouseOut={() => setSaveIconHover(false)}
+                                    className={`${classes.check} ${classes.editable}`}
+                                    value={`${saveIconHover ? "mdi-check-circle" : "mdi-check-circle-outline"} mdi-18px`}/>
+                        </td>
+                        <td className={classes.title}>
+                            <TextField placeholder="What's the task?" className={classes.editable}/>
+                        </td>
+                        <td className={classes.priority}>
+                            <PrioritySelector iconsOnly default="LOW"
+                                              className={"overriden"}/>
+                        </td>
+                        <td className={classes.deadline}>
+                            <DatePickerField style={{width: 120}} disablePast
+                                             format="MM/DD/YYYY"
+                                             default={moment()}
+                                             className={classes.editable}/>
+                        </td>
+                    </tr>
+                    }
+
                     {props.tasks.map((task, index) =>
                         <TaskRow key={task.id} data={task} index={index}/>
                     )}
-
-
                     {provided.placeholder}
                     </tbody>
                 }
