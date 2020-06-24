f9482b70a1f1404a214d6a85145cbddf43f60835
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-06-25 00:41:39 +0530

    Improve TaskIdGenerator if Sequences aren't supported

--- src/main/java/me/twodee/bux/Util/TaskIdGenerator.java
+++ src/main/java/me/twodee/bux/Util/TaskIdGenerator.java
@@ -17,7 +17,7 @@ import java.util.Properties;
 
 @Slf4j
 public class TaskIdGenerator extends SequenceStyleGenerator {
-
+    private static final String KEYSPACE = "project_";
     @Override
     public Serializable generate(SharedSessionContractImplementor session,
                                  Object object) throws HibernateException {
@@ -35,9 +35,48 @@ public class TaskIdGenerator extends SequenceStyleGenerator {
     private Long getLastId(SharedSessionContractImplementor session, String projectKey) throws SQLException {
         Connection connection = session.connection();
         Dialect dialect = session.getJdbcServices().getDialect();
+
+        if (dialect.supportsSequences()) {
+            return generateIdUsingSequences(dialect, connection, projectKey);
+        }
+        return generateIdWithSeparateTable(connection, projectKey);
+    }
+
+    private Long generateIdWithSeparateTable(Connection connection, String projectKey) throws SQLException {
+        connection.setAutoCommit(false);
+        connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+
+        try {
+            PreparedStatement fetchStmt = connection.prepareStatement("SELECT Val + 1 FROM Sequence WHERE SeqKey = ?");
+            fetchStmt.setString(1, KEYSPACE + projectKey);
+            ResultSet resultSet = fetchStmt.executeQuery();
+            resultSet.next();
+
+            long value = resultSet.getLong(1);
+
+            PreparedStatement updateStatement = connection.prepareStatement(
+                    "UPDATE Sequence SET Val = Val + 1 WHERE SeqKey = ?");
+            updateStatement.setString(1, KEYSPACE + projectKey);
+            updateStatement.executeUpdate();
+            connection.commit();
+            return value;
+
+        } catch (SQLException e) {
+            Statement creation = connection.createStatement();
+            creation.executeUpdate(
+                    "CREATE TABLE IF NOT EXISTS Sequence (SeqKey VARCHAR(30) PRIMARY KEY, Val INTEGER DEFAULT 0)");
+
+            PreparedStatement insert = connection.prepareStatement("INSERT INTO Sequence(SeqKey) VALUES (?)");
+            insert.setString(1, KEYSPACE + projectKey);
+            System.out.println("SUCCESS: " + insert.executeUpdate());
+            return generateIdWithSeparateTable(connection, projectKey);
+        }
+    }
+
+    private Long generateIdUsingSequences(Dialect dialect, Connection connection, String projectKey) throws SQLException {
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(
-                    dialect.getSequenceNextValString("project_" + projectKey));
+                    dialect.getSequenceNextValString(KEYSPACE + projectKey));
             ResultSet resultSet = preparedStatement.executeQuery();
             resultSet.next();
             return resultSet.getLong(1);
@@ -46,12 +85,12 @@ public class TaskIdGenerator extends SequenceStyleGenerator {
             // The Sequence doesn't exist, create and retry
             Statement statement = connection.createStatement();
             connection.setAutoCommit(false);
-            for (String command : dialect.getCreateSequenceStrings("project_" + projectKey, 1, 1)) {
+            for (String command : dialect.getCreateSequenceStrings(KEYSPACE + projectKey, 1, 1)) {
                 statement.addBatch(command);
             }
             statement.executeBatch();
             connection.commit();
-            return getLastId(session, projectKey);
+            return generateIdUsingSequences(dialect, connection, projectKey);
         }
     }
 
