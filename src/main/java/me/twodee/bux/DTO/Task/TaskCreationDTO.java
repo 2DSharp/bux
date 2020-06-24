3b198bf1f82f98c873e5e3023db648eca69b29f4
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-06-24 22:46:58 +0530

    Implement Task creation

---
+++ src/main/java/me/twodee/bux/DTO/Task/TaskCreationDTO.java
@@ -0,0 +1,38 @@
+package me.twodee.bux.DTO.Task;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import me.twodee.bux.DTO.DataTransferObject;
+import me.twodee.bux.Model.Entity.Task;
+
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TaskCreationDTO extends DataTransferObject {
+
+    @NotBlank(message = "{validation.task.title.empty}")
+    @Size(max = 100, message = "{validation.task.title.too_long}")
+    private String title;
+
+    @Size(max = 200, message = "{validation.task.description.too_long}")
+    private String description;
+
+    @FutureOrPresent(message = "{validation.task.deadline.past}")
+    private LocalDate deadline;
+
+    private int assignee;
+
+    private Task.Priority priority;
+
+    private int goalId;
+
+    private String projectKey;
+
+    private String status;
+}
---
+++ src/main/java/me/twodee/bux/Model/Service/TaskService.java
@@ -0,0 +1,63 @@
+package me.twodee.bux.Model.Service;
+
+import me.twodee.bux.DTO.Task.TaskCreationDTO;
+import me.twodee.bux.DTO.Task.TaskDTO;
+import me.twodee.bux.Factory.NotificationFactory;
+import me.twodee.bux.Model.Entity.Project;
+import me.twodee.bux.Model.Entity.Task;
+import me.twodee.bux.Model.Entity.User;
+import me.twodee.bux.Model.Repository.TaskRepository;
+import me.twodee.bux.Provider.SpringHelperDependencyProvider;
+import me.twodee.bux.Util.DomainToDTOConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.validation.ConstraintViolation;
+import java.util.Set;
+
+@Service
+public class TaskService {
+    private final TaskRepository repository;
+    private SpringHelperDependencyProvider provider;
+    private ProjectManagement projectManagement;
+
+    @Autowired
+    public TaskService(TaskRepository repository,
+                       SpringHelperDependencyProvider provider,
+                       ProjectManagement projectManagement
+    ) {
+        this.repository = repository;
+        this.provider = provider;
+        this.projectManagement = projectManagement;
+    }
+
+    private TaskDTO createTask(TaskCreationDTO dto, User user) {
+        Set<ConstraintViolation<TaskCreationDTO>> violations = provider.getValidator().validate(dto);
+        if (!violations.isEmpty()) {
+            dto.setNotification(DomainToDTOConverter.convert(violations));
+            return null;
+        }
+
+        if (!projectManagement.projectExists(dto.getProjectKey())) {
+            dto.appendNotification(NotificationFactory.createErrorNotification("global",
+                                                                               provider.getMessageByLocaleService().getMessage(
+                                                                                       "validation.project.key.nonexistent")));
+            return null;
+        }
+
+        Project project = new Project(dto.getProjectKey());
+        Task task = Task.builder()
+                .project(project)
+                .title(dto.getTitle())
+                .deadline(dto.getDeadline())
+                .priority(dto.getPriority())
+                .createdBy(user)
+                .status(dto.getStatus())
+                .description(dto.getDescription())
+                .assignee(new User(dto.getAssignee()))
+                .build();
+
+        task = repository.save(task);
+        return TaskDTO.build(task);
+    }
+}
