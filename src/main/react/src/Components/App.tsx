5a419a7a2e3f476a7609d199abde1d94357ccb06
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-06-28 21:06:25 +0530

    Clear up App.tsx

--- src/main/react/src/Components/App.tsx
+++ src/main/react/src/Components/App.tsx
@@ -1,8 +1,7 @@
-import React, {ChangeEvent, FormEvent, Suspense, useState} from 'react';
+import React, {Suspense} from 'react';
 import 'bulma/css/bulma.css'
 import '../sass/base.scss'
 import '../less/app.less'
-import Axios from "axios";
 
 import Login from "./Page/Login";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
@@ -12,28 +11,7 @@ import CreateNewProject from "./WorkspaceContent/CreateNewProject";
 import Loading from "./Page/Loading";
 
 
-const App = (props: any) => {
-
-    const [projects, setProjects] = useState('');
-    const [projName, setProjName] = useState('');
-    const updateProjectName = (event: ChangeEvent<HTMLInputElement>) => {
-        setProjName(event.target.value);
-    };
-    Axios.get('/issues')
-        .then(response => {
-            //setIssues(response.data.msg);
-            setProjects(response.data);
-        });
-
-    const addNewIssue = (event: FormEvent) => {
-        event.preventDefault();
-        Axios.post('/issue', {
-            name: projName
-        })
-            .then(response => {
-                console.log(response.data)
-            });
-    };
+const App = () => {
 
     const Projects = React.lazy(() => import('./Page/Projects'));
 
