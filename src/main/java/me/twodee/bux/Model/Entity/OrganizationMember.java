086e66af160d5ddafc1ff751921d8177b4cb4f6e
2dsharp <30053071+2DSharp@users.noreply.github.com>
2020-08-05 18:55:26 +0530

    Add role and instantiation mechanism

--- src/main/java/me/twodee/bux/Model/Entity/OrganizationMember.java
+++ src/main/java/me/twodee/bux/Model/Entity/OrganizationMember.java
@@ -1,8 +1,14 @@
 package me.twodee.bux.Model.Entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class OrganizationMember {
     public enum Role {
         READ, // Can view and track the organization
@@ -20,5 +26,12 @@ public class OrganizationMember {
     @ManyToOne
     private User user;
 
+    @NotNull
+    private Role role;
 
+    public OrganizationMember(Organization organization, User user, @NotNull Role role) {
+        this.organization = organization;
+        this.user = user;
+        this.role = role;
+    }
 }
