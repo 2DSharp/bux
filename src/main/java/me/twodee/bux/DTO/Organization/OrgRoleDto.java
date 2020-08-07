12c90f00855e179add4d51221a8af7053b534ff5
2dsharp <30053071+2DSharp@users.noreply.github.com>
2020-08-07 18:27:43 +0530

    Add org role

---
+++ src/main/java/me/twodee/bux/DTO/Organization/OrgRoleDto.java
@@ -0,0 +1,18 @@
+package me.twodee.bux.DTO.Organization;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+import me.twodee.bux.DTO.DataTransferObject;
+import me.twodee.bux.DTO.User.PublicUserDTO;
+import me.twodee.bux.Model.Entity.OrganizationMember;
+
+@Builder
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@Getter
+public class OrgRoleDto extends DataTransferObject {
+    private String orgName;
+    private OrganizationMember.Role role;
+    private PublicUserDTO target;
+    private PublicUserDTO modifier;
+}
