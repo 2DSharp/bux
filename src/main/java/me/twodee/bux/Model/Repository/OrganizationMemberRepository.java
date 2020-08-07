272d617eb5a532f609b5cd595dbdee5f1e757ca9
2dsharp <30053071+2DSharp@users.noreply.github.com>
2020-08-08 03:39:03 +0530

    Add repository methods for fetching org data

--- src/main/java/me/twodee/bux/Model/Repository/OrganizationMemberRepository.java
+++ src/main/java/me/twodee/bux/Model/Repository/OrganizationMemberRepository.java
@@ -1,9 +1,12 @@
 package me.twodee.bux.Model.Repository;
 
+import me.twodee.bux.Model.Entity.Organization;
 import me.twodee.bux.Model.Entity.OrganizationMember;
+import me.twodee.bux.Model.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrganizationMemberRepository extends JpaRepository<OrganizationMember, Integer> {
+    OrganizationMember findByOrganizationAndUser(Organization organization, User user);
 }
