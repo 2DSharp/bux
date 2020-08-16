565d3f8ff6d78740a5f58fb7c34b0164b1b1f7bb
2dsharp <30053071+2DSharp@users.noreply.github.com>
2020-08-17 03:17:39 +0530

    Optimize imports

--- src/main/java/me/twodee/bux/Component/Authorization/AuthAspect.java
+++ src/main/java/me/twodee/bux/Component/Authorization/AuthAspect.java
@@ -1,11 +1,9 @@
 package me.twodee.bux.Component.Authorization;
 
 import me.twodee.bux.Model.Service.AccountService;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
