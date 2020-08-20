b8851c22836a339e0acbd65523ad98db1a8083f0
2dsharp <30053071+2DSharp@users.noreply.github.com>
2020-08-21 01:21:11 +0530

    Fix container

--- src/main/react/src/Components/Layout/Container.tsx
+++ src/main/react/src/Components/Layout/Container.tsx
@@ -4,10 +4,8 @@ import "../../sass/project.scss";
 const Container = (props : {children : ReactNode}) => {
     return (
             <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-                <div className="centerpiece">
-                    <div className="container">
+                <div className="container centerpiece">
                             {props.children}
-                    </div>
                 </div>
             </div>
     );
