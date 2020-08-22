2d5dad3ef18ca22cfaafc0d216b0c65f4d7e9b6a
2dsharp <30053071+2DSharp@users.noreply.github.com>
2020-08-23 02:01:48 +0530

    Add new validation rules

--- src/main/react/src/service/validator.ts
+++ src/main/react/src/service/validator.ts
@@ -5,8 +5,24 @@ function isRequiredSuccess(value: string, param: boolean) {
     return true;
 }
 
+function isLengthRangeSuccess(value: string, param: {min?: number, max?: number}) {
+    let success = true;
+    if (param.min)
+        success = success && value.trim().length >= param.min;
+    if (param.max)
+        success = success && value.trim().length <= param.max;
+
+    return success;
+}
+
+function isPatternSuccess(value: string, regExp: RegExp) {
+    return regExp.test(value);
+}
+
 const isValid: any = {
     required: isRequiredSuccess,
+    length: isLengthRangeSuccess,
+    pattern: isPatternSuccess
 }
 
 function validateField(rules: any, value: any) {
@@ -29,6 +45,7 @@ export default function validate(values: object, rules: any) {
     let messages: any = {};
 
     for (let [key, value] of Object.entries(values)) {
+        console.log()
         if (rules[key]) {
             const message = validateField(rules[key], value)
             if (message) {
