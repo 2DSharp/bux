34a6bc07bfeab8c215007ad8715fe9d7bf06670f
2dsharp <30053071+2DSharp@users.noreply.github.com>
2020-08-22 16:37:03 +0530

    Move browser router up for accessing location in App

--- src/main/react/src/index.tsx
+++ src/main/react/src/index.tsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
 import App from "./Components/App";
+import {BrowserRouter} from "react-router-dom";
 
 axios.defaults.baseURL = 'http://localhost:2310/api/';
 //axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
 axios.defaults.headers.post['Content-Type'] = 'application/text';
 axios.defaults.withCredentials = true;
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
