ac9639c18dd59f0b6854a3c1b79954d491961c15
2dsharp <30053071+2DSharp@users.noreply.github.com>
2020-08-23 13:06:24 +0530

    Add size customization to spinner

--- src/main/react/src/Components/WorkspaceContent/SpinLoader.tsx
+++ src/main/react/src/Components/WorkspaceContent/SpinLoader.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import GeneralSpin from "../Element/Loader/GeneralSpin";
 
-const SpinLoader = () => {
+const SpinLoader = (props: {bodySize?: number, size?: number}) => {
+    let style = {
+        height: 400
+    };
+    if (props.bodySize) {
+        style = {
+            height: props.bodySize
+        }
+    }
     return (
-        <div className="centered-absolutely" style={{height: 400}}>
-            <GeneralSpin size={60}/>
+        <div className="centered-absolutely" style={style}>
+            <GeneralSpin size={props.size ? props.size : 60}/>
         </div>
     );
 };
