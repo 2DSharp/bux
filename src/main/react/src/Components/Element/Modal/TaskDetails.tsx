22bc50d3e9e4d0a671aca035239d7ae225104f76
2dsharp <30053071+2DSharp@users.noreply.github.com>
2020-11-23 00:50:33 +0530

    Update Task Details design
    Signed-off-by: 2dsharp <30053071+2DSharp@users.noreply.github.com>

--- src/main/react/src/Components/Element/Modal/TaskDetails.tsx
+++ src/main/react/src/Components/Element/Modal/TaskDetails.tsx
@@ -3,7 +3,6 @@ import {Modal} from "antd";
 import {TaskData} from "../../../types";
 import {makeStyles} from "@material-ui/styles";
 import PrioritySelector from "../Form/PrioritySelector";
-import MarkdownIt from 'markdown-it'
 import variables from "../../../sass/colors.module.scss"
 import UserCardMin from "../Misc/UserCardMin";
 import TextField from "../Form/TextField";
@@ -11,6 +10,8 @@ import FormData from "../Form/FormData";
 import Editor from "rich-markdown-editor";
 import base from "rich-markdown-editor/dist/theme";
 import {Constants} from "../../../constants";
+import PrimaryButton from "../Button/PrimaryButton";
+import Button from "../Button/Button";
 
 interface TaskDetailsProps {
     visible: boolean
@@ -27,7 +28,7 @@ const useStyles = makeStyles({
     },
     field: {
         display: "flex",
-        width: 320,
+        width: 280,
         margin: "10px 0px",
         fontSize: 14
     },
@@ -52,7 +53,21 @@ const useStyles = makeStyles({
     },
     description: {
         margin: "5px 0",
-        paddingTop: 10
+        paddingTop: 10,
+    },
+    descriptionBody: {
+        "&:after": {
+            content: "''",
+            marginTop: 5,
+            display: "block",
+            borderBottom: `1px solid ${variables.primary}`,
+            transform: "scaleX(0)",
+            transition: "transform 250ms ease-in-out",
+            transformOrigin: "0% 50%"
+        },
+        "&:hover:after": {
+            transform: "scaleX(1)"
+        }
     },
     body: {
         padding: "10px 15px"
@@ -62,21 +77,28 @@ const useStyles = makeStyles({
         marginBottom: 10
     },
     editor: {
-        margin: "10px 30px",
+        margin: "10px 0",
     },
     descriptionField: {
-        margin: "5px 0",
+        margin: "5px 0 5px 0",
         maxHeight: 150,
         overflowY: "auto",
-        borderRadius: 4,
-        border: "1px solid rgba(0,0,0,0)",
-        "&:hover": {
-            borderColor: variables.primaryLight,
-        },
-        transition: "all 0.2s ease-in",
+    },
+    descriptionEditor: {
+        padding: "0 25px",
+        borderBottom: `1px solid ${variables.primary}`,
+        marginBottom: 10
+    },
+    descriptionActions: {
+        textAlign: "right",
+        "& > button": {
+            fontSize: 14,
+            borderRadius: 4,
+            margin: "0 5px"
+        }
     }
 })
-const Field = (props: { name: string, value: string | ReactNode, edit?: ReactNode }) => {
+const Field = (props: { name: string, value: string | ReactNode, edit?: ReactNode, editable?: boolean }) => {
     const styles = useStyles();
     const [editing, setEditing] = useState(false);
     const ref = useRef();
@@ -100,18 +122,24 @@ const Field = (props: { name: string, value: string | ReactNode, edit?: ReactNod
                 <TextField autoFocus onFocus={handleFocus} forwardRef={ref} onBlur={() => setEditing(false)}
                            defaultValue={props.value}/>
             </FormData>
-            : <div onClick={() => setEditing(true)} className={`${styles.fieldCol} ${styles.fieldValue}`}>
+            : <div onClick={() => props.editable && setEditing(true)}
+                   className={`${styles.fieldCol} ${props.editable && styles.fieldValue}`}>
                 {props.value}
             </div>}
     </div>
 };
 const TaskDetails = (props: TaskDetailsProps) => {
     const {data} = props;
-    console.log(data);
     const styles = useStyles();
 
-    const mdParser = new MarkdownIt(/* Markdown-it options */);
+    const [descriptionEditorOn, setDescriptionEditorOn] = useState(false);
 
+    const showDescriptionEditor = () => {
+        setDescriptionEditorOn(true);
+    }
+    const submitNewDescription = () => {
+        setDescriptionEditorOn(false);
+    }
     return (
         <Modal
             centered
@@ -121,29 +149,50 @@ const TaskDetails = (props: TaskDetailsProps) => {
             visible={props.visible}
             okButtonProps={{style: {display: 'none'}}}
             onCancel={() => props.setModalVisible(false)}
+            destroyOnClose
+            afterClose={() => setDescriptionEditorOn(false)}
         >
             <div className={styles.body}>
-                <h2>{data.title}</h2>
+                <h2 style={{fontWeight: 500, fontFamily: "Poppins, Arial, serif"}}>{data.title}</h2>
                 <div className={styles.description}>
                     <b>Description</b>
                     <div className={styles.descriptionField}>
-                        <Editor
-                            theme={{...base, zIndex: Constants.zIndex.modal + 100}}
-                            defaultValue={data.description && data.description}
-                            onChange={(value) => console.log(value)}
-                            placeholder={"Add a description..."}
-                            className={styles.editor}
-                        />
+                        {descriptionEditorOn ?
+                            <div className={styles.descriptionEditor}>
+                                <Editor
+                                    ref={input => input && input.focusAtEnd()}
+                                    key={data.id}
+                                    theme={{...base, zIndex: Constants.zIndex.modal + 100}}
+                                    defaultValue={data.description}
+                                    onChange={(value) => console.log(value)}
+                                    placeholder={"Add a description..."}
+                                    className={styles.editor}
+                                />
+                            </div>
+                            :
+                            <div className={styles.descriptionBody}
+                                 onClick={showDescriptionEditor}>{data.description ? data.description :
+                                "Add a description..."}</div>
+                        }
+                    </div>
+                    {descriptionEditorOn &&
+                    <div className={styles.descriptionActions}>
+                        <Button className={`${styles.descriptionActions} is-light`}>Cancel</Button>
+                        <PrimaryButton className={styles.descriptionActions}>Save</PrimaryButton>
+                    </div>
+                    }
+
+                </div>
+                <div>
+                    <div className={styles.detailFields}>
+                        <Field editable name={"Created By"} value={<UserCardMin user={data.createdBy}/>}/>
+                        {data.assignee &&
+                        <Field editable name={"Assignee"} value={<UserCardMin user={data.assignee}/>}/>}
+                        <Field name={"Priority"}
+                               value={<PrioritySelector style={{padding: 5, flexGrow: 1,}} default={data.priority}/>}/>
+                        <Field editable name={"Deadline"} value={data.deadline}/>
                     </div>
                 </div>
-                <p>
-                    <div className={styles.detailFields}>
-                        <Field name={"Created By"} value={<UserCardMin user={data.createdBy}/>}/>
-                        {data.assignee && <Field name={"Assignee"} value={<UserCardMin user={data.assignee}/>}/>}
-                        <Field name={"Priority"} value={<PrioritySelector default={data.priority}/>}/>
-                        <Field name={"Deadline"} value={data.deadline}/>
-                    </div>
-                </p>
             </div>
         </Modal>
     );
